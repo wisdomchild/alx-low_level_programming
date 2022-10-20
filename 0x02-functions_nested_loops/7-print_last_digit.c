e7c540ad3e6c809dce38f6ef990e9ba15c650777
wisdomchild <priscaneb@gmail.com>
2022-10-19 17:16:38 -0700

    my 7-print

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a numbe
+ * @x: int type number
+ * Return: return value of last digit
+ */
+
+
+int print_last_digit(int x)
+{
+	int y;
+
+	if (x < 0)
+	{
+		y = -1 * (x % 10);
+		_putchar(y + '0');
+		return (y);
+	}
+	else
+	{
+		y = x % 10;
+		_putchar(y + '0');
+		return (y);
+	}
+}
